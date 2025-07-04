e7de428b57beda6e6a25e17b44af64e07398652b
WIpzx <juanpuro9@gmail.com>
2025-07-04 18:46:11 -0400

    En general agregue el comienzo del apartado de html al proyecto

--- villa_verde/talleres/views.py
+++ villa_verde/talleres/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .models import Taller, Categoria
+from django.utils import timezone
 
-# Create your views here.
+def home(request):
+    categoria_id = request.GET.get('categoria')
+    talleres = Taller.objects.filter(
+        estado='aceptado',
+        fecha__gte=timezone.now()
+    )
+    if categoria_id:
+        talleres = talleres.filter(categoria_id=categoria_id)
+    categorias = Categoria.objects.all()
+    return render(request, 'talleres/home.html', {
+        'talleres': talleres,
+        'categorias': categorias,
+        'categoria_actual': categoria_id
+    })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'talleres/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
