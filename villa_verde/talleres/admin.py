e1b433c31357ae3ae77ec961ba211bd86789a2ab
Aikodv <112638686+Aikodv@users.noreply.github.com>
2025-07-06 22:29:14 -0400

    panel de administracion

--- villa_verde/talleres/admin.py
+++ villa_verde/talleres/admin.py
@@ -2,8 +2,35 @@ from django.contrib import admin
 from .models import Usuario, Lugar, Categoria, Profesor, Taller
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Usuario, UserAdmin)
-admin.site.register(Lugar)
-admin.site.register(Categoria)
-admin.site.register(Profesor)
-admin.site.register(Taller)
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = ('username', 'email', 'tipo', 'is_staff', 'is_superuser')
+    list_filter = ('tipo', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+
+
+@admin.register(Lugar)
+class LugarAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion')
+    search_fields = ('nombre',)
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+    search_fields = ('nombre',)
+
+
+@admin.register(Profesor)
+class ProfesorAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo',)
+    search_fields = ('nombre_completo',)
+
+
+@admin.register(Taller)
+class TallerAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha', 'duracion_horas', 'estado', 'profesor', 'lugar', 'categoria')
+    list_filter = ('estado', 'fecha', 'categoria')
+    search_fields = ('titulo', 'profesor__nombre_completo')
+    ordering = ('fecha',)
