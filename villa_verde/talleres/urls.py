cddec819b986f4d7cf9986d01a5874005edc7b03
Aikodv <112638686+Aikodv@users.noreply.github.com>
2025-07-07 00:13:11 -0400

    Api rest

--- villa_verde/talleres/urls.py
+++ villa_verde/talleres/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from . import api_views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,6 +10,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('proponer/', views.proponer_taller_view, name='proponer_taller'),
-
+    path('talleres/', api_views.listar_talleres_api, name='listar_talleres_api'),
     
 ]
